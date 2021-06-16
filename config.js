12bf5fdab3b5dedd5585d0e4ba5a2792b7072be8
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-16 16:59:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-exports.TOKEN = '';
+exports.TOKEN = 'ODM5MjY0ODA2Nzc5MDkzMDAz.YJHIQg.l6RDAYRNRhRXCeabiA6z_SFAL28';
 
-exports.PREFIX = '';
+exports.PREFIX = '1';
 
-exports.news_API = '';
+exports.news_API = 'd1f98e5a72054d1f956b20d5e3729a9c';
 
-exports.giphy_API = '';
+exports.giphy_API = 'J6uSs68pupRQEfPHFXq6G9ggLQt0akQr';
 
-exports.AME_API = '';
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
